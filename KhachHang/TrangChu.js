fcbca9836be4c375759f92e96a0eb364bf8764bd
Văn Hoàn <hoan0767021265@gmail.com>
2022-11-06 20:54:18 +0700

    fixbug

---
+++ KhachHang/TrangChu.js
@@ -0,0 +1,8 @@
+function TimKiem() {
+    document.write(
+        '<form action="" class="form_TimKiem">'+
+            '<input type="text" placeholder="Nhập sản phẩm cần tìm" id="search-box" >'+
+            '<label for="search-box" class="icon"></label>'+
+        '</form>'
+    );
+}
