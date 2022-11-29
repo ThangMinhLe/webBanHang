6b1d623dee83a9c885ea72bfe65178e38aecaed8
Văn Hoàn <hoan0767021265@gmail.com>
2022-11-29 16:56:21 +0700

    fix

--- KhachHang/ChiTietSP.js
+++ KhachHang/ChiTietSP.js
@@ -1,4 +1,5 @@
 function LuuThongTin_ChiTiet_SP(i) {
+    localStorage.removeItem('sp_tam');
     var DSSP = JSON.parse(localStorage.getItem('DSSP'));
     localStorage.setItem("sp_tam", JSON.stringify([DSSP[i]]));
 }
