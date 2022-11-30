d492b0f894aac0c07d1a29492f766e512df6ffe0
Văn Hoàn <hoan0767021265@gmail.com>
2022-11-30 20:11:52 +0700

    fix hienthisp

--- KhachHang/TrangChu.js
+++ KhachHang/TrangChu.js
@@ -1,10 +1,29 @@
+function hienthi_sp(danhmuc){
+    var DSSP = JSON.parse(localStorage.getItem('DSSP'));
+    for (var i = 0; i < DSSP.length; i++) {
+        if (DSSP[i].LoaiSP == danhmuc) {
+            document.write(
+                '<div class="SP">' +
+                '<img src="' + DSSP[i].HinhAnh + '"' + 'class="card__img">' +
+                '<img src="img/NhanDan.png" alt="" class="card_NhanDan">' +
+                '<div class="card__data">' +
+                '<h1 class="TenSP">' + DSSP[i].TenSP + '</h1>' +
+                '<span class="GiaTien">' + DSSP[i].GiaBan + '</span>' +
+                '<a href="./KhachHang/GioHang.html" class="Buy" onclick="LuuThongTinSP_GioHang(' + i + ')">Buy Now</a>' +
+                '<a href="./KhachHang/ChiTietSP.html" class="ChiTiet" onclick="LuuThongTin_ChiTiet_SP(' + i + ')">Chi tiết</a>' +
+                '</div>' +
+                '</div>'
+            );
 
+        }
+    }
+}
 function TimKiem() {
     var DSSP = JSON.parse(localStorage.getItem('DSSP'));
     var s = document.getElementById("tkiem").value;
     localStorage.removeItem('DSTKiem');
     DSTKiem = new Array();
-    if (s != null || s!="") {
+    if (s != null || s != "") {
         if (!isNaN(s) && s > 0) {
             for (var i = 0; i < DSSP.length; i++) {
                 if (DSSP[i].GiaBan <= Number(s)) {
@@ -14,7 +33,7 @@ function TimKiem() {
             }
         } else {
             for (var i = 0; i < DSSP.length; i++) {
-                if (DSSP[i].MaSP.toLowerCase().search(s.toLowerCase())!=-1|| DSSP[i].TenSP.toLowerCase().search(s.toLowerCase())!=-1 || DSSP[i].LoaiSP.toLowerCase().search(s.toLowerCase())!=-1) {
+                if (DSSP[i].MaSP.toLowerCase().search(s.toLowerCase()) != -1 || DSSP[i].TenSP.toLowerCase().search(s.toLowerCase()) != -1 || DSSP[i].LoaiSP.toLowerCase().search(s.toLowerCase()) != -1) {
                     DSTKiem.push(DSSP[i]);
                     localStorage.setItem("DSTKiem", JSON.stringify(DSTKiem));
                 }
@@ -38,22 +57,23 @@ function LuuThongTinSP_GioHang(i) {
     var GioHang = JSON.parse(localStorage.getItem('GioHang'));
     if (GioHang == null) {
         GioHang = new Array();
-        var tien=1*Number(DSTKiem[i].GiaBan);
-        sp = { MaSP: DSTKiem[i].MaSP, TenSP: DSTKiem[i].TenSP, GiaBan: DSTKiem[i].GiaBan, HinhAnh: DSTKiem[i].HinhAnh, SoLuongMua: '1' ,ThanhTien: tien};
+        var tien = 1 * Number(DSTKiem[i].GiaBan);
+        sp = { MaSP: DSTKiem[i].MaSP, TenSP: DSTKiem[i].TenSP, GiaBan: DSTKiem[i].GiaBan, HinhAnh: DSTKiem[i].HinhAnh, SoLuongMua: '1', ThanhTien: tien };
     } else {
         tam = DSTKiem[i].MaSP;
         var SoLuong = 0;
         for (var j = 0; j < GioHang.length; j++) {
             if (GioHang[j].MaSP == tam) {
                 SoLuong = Number(GioHang[j].SoLuongMua) + 1;
-                GioHang.splice(j,1);
+                GioHang.splice(j, 1);
             } else {
                 SoLuong = 1;
             }
         }
-        var tien=Number(SoLuong)*Number(DSTKiem[i].GiaBan);
-        sp = { MaSP: DSTKiem[i].MaSP, TenSP: DSTKiem[i].TenSP, GiaBan: DSTKiem[i].GiaBan, HinhAnh: DSTKiem[i].HinhAnh, SoLuongMua: SoLuong ,ThanhTien: tien};
+        var tien = Number(SoLuong) * Number(DSTKiem[i].GiaBan);
+        sp = { MaSP: DSTKiem[i].MaSP, TenSP: DSTKiem[i].TenSP, GiaBan: DSTKiem[i].GiaBan, HinhAnh: DSTKiem[i].HinhAnh, SoLuongMua: SoLuong, ThanhTien: tien };
     }
     GioHang.push(sp);
     localStorage.setItem("GioHang", JSON.stringify(GioHang));
 }
+
