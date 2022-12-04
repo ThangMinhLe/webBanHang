f3a22b8fedc25d11eba124200fdab7535d65e25e
LaosPhu <119502732+LaosPhu@users.noreply.github.com>
2022-12-04 20:03:57 +0700

    thinh thanh toan

--- KhachHang/ThanhToan.js
+++ KhachHang/ThanhToan.js
@@ -2,7 +2,5 @@ function laysoluongsp ()
 { 
     var DSSP = JSON.parse(localStorage.getItem('DSSP'));
     var GioHang = JSON.parse(localStorage.getItem('GioHang'));
-    
-   
    alert("check");
 }
