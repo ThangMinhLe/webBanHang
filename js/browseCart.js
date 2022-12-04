dd90ddb4d2cbf91aa642579a915621c646a7bedf
ThuanChannChann <thuanann.sama@gmail.com>
2022-12-04 21:48:05 +0700

    gio hang

--- js/browseCart.js
+++ js/browseCart.js
@@ -219,7 +219,6 @@ function xemChiTiet(maDonHang){
         if(duyet.maDonHang === maDonHang) {
             (DuyetDonHang[index].chiTiet).forEach((ct, dem) => {
                 tongTien += Number(ct.GiaBan)*Number(ct.SoLuongMua);
-
                 tableContent += `<tr>
                 <td>${dem + 1}</td>
                 <td>${ct.MaSP}</td>
@@ -250,7 +249,6 @@ function tienVN(giaTri){
     return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(giaTri);
 }
 
-
 function checkDH(e) {
    let row = e.parentElement.parentElement.parentElement;
    let id =  row.cells[1].innerText;
@@ -272,4 +270,3 @@ function toggleSwitch(e) {
     let row = e.parentElement.parentElement.parentElement;
 
 }
-
