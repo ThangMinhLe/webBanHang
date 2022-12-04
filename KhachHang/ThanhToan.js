79f9b2b48b96caecc5441b8cf945dc22c5aacfaf
LaosPhu <119502732+LaosPhu@users.noreply.github.com>
2022-12-04 17:16:35 +0700

    thinh

---
+++ KhachHang/ThanhToan.js
@@ -0,0 +1,8 @@
+function laysoluongsp ()
+{ 
+    var DSSP = JSON.parse(localStorage.getItem('DSSP'));
+    var GioHang = JSON.parse(localStorage.getItem('GioHang'));
+    
+   
+   alert("check");
+}
