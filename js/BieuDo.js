faa9a271dfd0d2d61a2782db9012a3b0f3cacbbb
HuynhQuocTien <115643869+HuynhQuocTien@users.noreply.github.com>
2022-12-06 00:12:55 +0700

    chuyen tu Trangchu sang Admin

--- js/BieuDo.js
+++ js/BieuDo.js
@@ -1,25 +1,73 @@
 "use strict";
-// var tongDT =
+var DonDuyet = localStorage.getItem('DuyetDonHang') ? JSON.parse(localStorage.getItem('DuyetDonHang')) : [];
+var listDSSP = localStorage.getItem('DSSP') ? JSON.parse(localStorage.getItem('DSSP')) : [];
+
+let i;
+let j;
+let DoanhThu=0;
+let TienBanHang=0;
+let SLSPDaBan=0;
+let TongSP=0;
+
+for (i=0;i<DonDuyet.length;i++){
+    // Tinh so luong sp da ban
+    for (j=0;j < DonDuyet[i].chiTiet.length;j++){
+        if (typeof DonDuyet[i].chiTiet[j].SoLuongMua == "string")
+            SLSPDaBan += Number(DonDuyet[i].chiTiet[j].SoLuongMua);
+        else if (typeof DonDuyet[i].chiTiet[j].SoLuongMua == "number")
+            SLSPDaBan += Number(DonDuyet[i].chiTiet[j].SoLuongMua);
+    }
+    //Tinh Tien da ban
+    if (typeof DonDuyet[i].tongTien == "string" ) {
+        TienBanHang += Number(DonDuyet[i].tongTien);
+    }
+    else if ((typeof DonDuyet[i].tongTien) == "number" ) {
+        TienBanHang += DonDuyet[i].tongTien;
+    }
+}
+
+
+for (i=0;i<listDSSP.length;i++){
+    if ((typeof listDSSP[i].SoLuong)== 'string' ){
+        TongSP+=Number(listDSSP[i].SoLuong);
+        DoanhThu += Number(listDSSP[i].SoLuong)*Number(listDSSP[i].GiaBan);
+    }
+    else if ((typeof listDSSP[i].SoLuong) == 'number' ) {
+        TongSP+=Number(listDSSP[i].SoLuong);
+        DoanhThu += Number(listDSSP[i].SoLuong * listDSSP[i].GiaBan);
+    }
+}
+
+let TileDT = TienBanHang*100/DoanhThu;
+
+let TileSPban= SLSPDaBan*100/TongSP;
+
+
 const progressBar = document.querySelector(".circular__progress--bar");
 const progressValue=document.querySelector(".circular__progress--value");
+
 let initValue = 0;
-let finalValue = 50 ;
+let finalValue = Number(TileSPban.toFixed(0));
+console.log(finalValue);
+
 const updateValue = function (){
     if(initValue!== finalValue){
         initValue++;
         progressValue.textContent = `${initValue}%`;
-        progressBar.style.backgroundImage= `conic-gradient(#74c0fc,#4dabf7,#339af0 ${3.6 * initValue}deg,#e9ecef 0)`
+        progressBar.style.backgroundImage= `conic-gradient(#74c0fc,#4dabf7,#339af0 ${3.6 * initValue}deg,#e9ecef 0)`;
     }
     else
         clearInterval();
 };
 
-setInterval(updateValue,20);
+setInterval(updateValue,50);
+
+
 
 const progressBar2 = document.querySelector(".circular__progress--bar-2");
 const progressValue2=document.querySelector(".circular__progress--value-2");
 let initValue2 = 0;
-let finalValue2 = 50 ;
+let finalValue2 = Number(TileDT.toFixed(0)) ;
 const updateValue2 = function (){
     if(initValue2!== finalValue2){
         initValue2++;
@@ -30,4 +78,4 @@ const updateValue2 = function (){
         clearInterval();
 };
 
-setInterval(updateValue2,20);
+setInterval(updateValue2,50);
--- js/ShowUser.js
+++ js/ShowUser.js
@@ -1,9 +1,19 @@
 var UserLogin = localStorage.getItem('UserLogin') ? JSON.parse(localStorage.getItem('UserLogin')) : [];
-if ( UserLogin.length !== '0'){
-    let chunk = UserLogin.name.split(" ");
-    console.log(chunk);
-    document.getElementById('NameUser').innerText= 'Welcome  ' + chunk[chunk.length-1];
-    document.getElementById('icon__show-login').style.visibility= 'hidden';
-    document.getElementById('icon__show-logout').style.visibility= 'visible';
-    document.getElementById('icon__show-logout').style.zIndex=1;
+
+console.log(JSON.stringify(UserLogin) !== '[]')
+
+if ( JSON.stringify(UserLogin) != '[]'){
+    if (UserLogin.name==='admin'){
+        document.getElementById('NameUser').innerText= 'Welcome admin' ;
+        document.getElementById('icon__show-login').style.visibility= 'hidden';
+        document.getElementById('icon__show-logout').style.visibility= 'visible';
+        document.getElementById('icon__show-logout').style.zIndex=1;
+    }
+    else {
+        let chunk = UserLogin.name.split(" ");
+        document.getElementById('NameUser').innerText = 'Welcome  ' + chunk[chunk.length - 1];
+        document.getElementById('icon__show-login').style.visibility = 'hidden';
+        document.getElementById('icon__show-logout').style.visibility = 'visible';
+        document.getElementById('icon__show-logout').style.zIndex = 1;
+    }
 }
